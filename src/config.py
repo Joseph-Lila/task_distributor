5d78fc5d9f581a3e69866939952dce5043c7d8b6
Artur Prakapenka <arturprokopenko01@gmail.com>
2023-01-24 21:39:02 +0300

    final attempt

--- src/config.py
+++ src/config.py
@@ -4,9 +4,6 @@ import pathlib
 THIS_DIR = pathlib.Path(__file__).parent.resolve().absolute()
 ROOT_DIR = THIS_DIR.parent
 
-for path in list(ROOT_DIR.rglob("*")):
-    print(path)
-
 
 def get_sqlite_connection_str():
     """
